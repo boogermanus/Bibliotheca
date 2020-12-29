22715432c052e34188ce68e934c33a7815e8e258
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-12-28 19:28:54 -0600

    add Library migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace Bibliotheca.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<Library> Libraries { get; set; }
+        
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
---
+++ Data/Migrations/20201229005143_AddLibrary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bibliotheca.Data.Migrations
+{
+    public partial class AddLibrary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Libraries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libraries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libraries");
+        }
+    }
+}

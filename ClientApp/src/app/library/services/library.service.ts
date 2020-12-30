2959a9097c358ca1bec4aee2888c0db670f6171d
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-12-29 18:56:52 -0600

    remove unused

--- ClientApp/src/app/library/services/library.service.ts
+++ ClientApp/src/app/library/services/library.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/services/api-service';
-import { LibraryModule } from '../library.module';
 import { ILibrary } from '../models/ilibrary';
 
 @Injectable()

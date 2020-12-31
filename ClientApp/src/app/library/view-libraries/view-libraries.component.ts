79fb835eb9d19d402783a7d08cd1702f4516348a
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-12-31 17:06:30 -0600

    async and unsubscribe

--- ClientApp/src/app/library/view-libraries/view-libraries.component.ts
+++ ClientApp/src/app/library/view-libraries/view-libraries.component.ts
@@ -10,24 +10,36 @@ import { LibraryService } from '../services/library.service';
   templateUrl: './view-libraries.component.html',
   styleUrls: ['./view-libraries.component.css']
 })
-export class ViewLibrariesComponent implements OnInit {
+export class ViewLibrariesComponent implements OnInit, OnDestroy {
 
-  libraries: ILibrary[] = [];
+  // libraries: ILibrary[] = [];
+  libraries: Observable<ILibrary[]>;
+  subscriptions: Subscription = new Subscription();
   constructor(private libraryService: LibraryService,
               public dialog: MatDialog) { }
 
-  ngOnInit(): void {
-      this.libraryService.getAll().subscribe(libraries => this.libraries = libraries);
+  public ngOnInit(): void {
+      // this.libraryService.getAll().subscribe(libraries => this.libraries = libraries);
+      this.libraries = this.libraryService.getAll();
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   public openAddDialog(): void {
     const dialogRef = this.dialog.open(AddLibraryModalComponent, {data: {name: ''}});
 
-    dialogRef.afterClosed().subscribe(name => {
-      this.libraryService
+    const sub = dialogRef.afterClosed().subscribe(name => {
+      const innerSub = this.libraryService
         .add(name)
-        .subscribe(newLibrary => this.libraries.push(newLibrary));
+        .subscribe(() => {
+          this.libraries = this.libraryService.getAll();
+        });
+      this.subscriptions.add(innerSub);
     });
+
+    this.subscriptions.add(sub);
   }
 
 }

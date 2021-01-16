30956f663dffc4eaf035c7e88d0fcc1a5192a2b5
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-01-15 20:47:06 -0600

    application user service

---
+++ ClientApp/src/app/services/application-user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IApplicationUser } from '../models/iapplication-user';
+import { ApiService } from './api-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationUserService {
+
+  private readonly applicationUserUrl = 'api/applicationuser';
+  constructor(private apiService: ApiService) { }
+
+  public getUsersForLibrary(libraryId: number): Observable<IApplicationUser> {
+    return this.apiService.get(`${this.applicationUserUrl}/usersforlibrary/${libraryId}`);
+  }
+}

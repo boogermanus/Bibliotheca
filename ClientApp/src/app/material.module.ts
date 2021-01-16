1ae52887268e6f8847ed2d9133bd522fee86c664
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-01-15 20:10:10 -0600

    linting

--- ClientApp/src/app/material.module.ts
+++ ClientApp/src/app/material.module.ts
@@ -5,7 +5,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatCardModule} from "@angular/material/card";
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],

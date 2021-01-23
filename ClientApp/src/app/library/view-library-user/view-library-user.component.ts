d6e7320204859695c454e7ca609d28a2c08888ab
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-01-22 18:39:06 -0600

    delete button basics

--- ClientApp/src/app/library/view-library-user/view-library-user.component.ts
+++ ClientApp/src/app/library/view-library-user/view-library-user.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { AuthorizeService, IUser } from 'src/api-authorization/authorize.service';
 import { IApplicationUser } from 'src/app/models/iapplication-user';
 import { ApplicationUserService } from 'src/app/services/application-user.service';
 
@@ -8,15 +9,23 @@ import { ApplicationUserService } from 'src/app/services/application-user.servic
   templateUrl: './view-library-user.component.html',
   styleUrls: ['./view-library-user.component.css']
 })
-export class ViewLibraryUserComponent implements OnInit {
+export class ViewLibraryUserComponent implements OnInit, OnDestroy {
 
   @Input()public libraryId: number;
   public users: Observable<IApplicationUser>;
+  public currentUser: IUser;
+  public subscriptions: Subscription = new Subscription();
 
-  constructor(private applicationUserService: ApplicationUserService) { }
+  constructor(private applicationUserService: ApplicationUserService,
+              private authorizeService: AuthorizeService) { }
 
   ngOnInit(): void {
     this.users = this.applicationUserService.getUsersForLibrary(this.libraryId);
+    this.subscriptions.add(this.authorizeService.getUser().subscribe(user => this.currentUser = user));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }

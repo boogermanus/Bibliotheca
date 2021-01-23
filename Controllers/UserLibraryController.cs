b79ec6089512049364b5b28fcb87becd9e91f98b
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-01-22 19:16:02 -0600

    add new controller

---
+++ Controllers/UserLibraryController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bibliotheca.Data;
+using Bibliotheca.Data.Repositories;
+using Bibliotheca.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace Bibliotheca.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserLibraryController
+    {
+
+    }
+}

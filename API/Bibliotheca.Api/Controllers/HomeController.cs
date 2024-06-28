bc4b0295869fe07215aadb6aa5ec7eb8a5199f62
Jordan <boogermanus@gmail.com>
2024-06-27 21:09:03 -0500

    add a home controller

---
+++ API/Bibliotheca.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bibliotheca.Controllers;
+[ApiController]
+[Route("/")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok($"Bibilotheca API {DateTime.Now}");
+    }
+}

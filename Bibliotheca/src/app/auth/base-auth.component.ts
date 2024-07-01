16aff5eabd0ca4762528f6ed1ad725e758f76c4b
Jordan <boogermanus@gmail.com>
2024-07-01 17:08:43 -0500

    remove dirty password hack...sort of

--- Bibliotheca/src/app/auth/base-auth.component.ts
+++ Bibliotheca/src/app/auth/base-auth.component.ts
@@ -11,7 +11,21 @@ export class BaseAuthComponent {
     public passwordsDoNotMatch = signal(false);
 
     constructor() { }
+
     protected isControlInvalid(control: AbstractControl, error: string = 'required'): boolean {
         return control.touched && control.hasError(error);
+    }
+
+    protected passwordValidator(control: AbstractControl): ValidationErrors {
+        const password = control.get('password').value as string;
+        const confirm = control.get('confirmPassword').value as string;
+
+        if(password.length === confirm.length || confirm.length >= password.length) {
+            if(password !== confirm) {
+                return {confirmPassword: true}
+            }
+        }
+
+        return null;
     }
 }

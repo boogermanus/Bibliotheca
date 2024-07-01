03d42782ee2d12c75b85be4ce9c212fef2a130c5
Jordan <boogermanus@gmail.com>
2024-06-30 19:57:44 -0500

    add database interfaces

---
+++ API/Bibliotheca.Core/Interfaces/Database/IBaseRepository.cs
@@ -0,0 +1,10 @@
+namespace Bibliotheca.Core.Interfaces.Database;
+
+public interface IBaseRepository<TEntity> where TEntity : class, IEntity
+{
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<TEntity> GetAsync(int id);
+    Task<TEntity> AddAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> DeleteAsync(int id);
+}
---
+++ API/Bibliotheca.Core/Interfaces/Database/IEntity.cs
@@ -0,0 +1,6 @@
+namespace Bibliotheca.Core.Interfaces.Database;
+
+public interface IEntity
+{
+    int Id { get; set; }
+}

6b9a127f3e465b740f49c734cd2809f7b57186b7
Jordan <boogermanus@gmail.com>
2024-06-30 19:49:20 -0500

    add auth controller methods

--- API/Bibliotheca.Api/Controllers/AuthController.cs
+++ API/Bibliotheca.Api/Controllers/AuthController.cs
@@ -1,3 +1,5 @@
+using Bibliotheca.Core.ApiModels.Auth;
+using Bibliotheca.Core.Interfaces.Auth;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bibliotheca.Controllers;
@@ -6,5 +8,34 @@ namespace Bibliotheca.Controllers;
 [Route("api/[controller]")]
 public class AuthController : ControllerBase 
 {
-    
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegistrationModel model)
+    {
+        var result = await _authService.Register(model);
+
+        if(result.Succeeded)
+            return Ok(true);
+
+        result.Errors.ToList().ForEach(e => ModelState.AddModelError(e.Code, e.Description));
+
+        return BadRequest(ModelState);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        var result = await _authService.Login(model);
+
+        if(result == null)
+            return Unauthorized(ModelState);
+
+        return Ok(result);
+    }
 }

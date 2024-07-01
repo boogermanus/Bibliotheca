c1577fceba53ce1cbf316f67fdff9cb818f741d7
Jordan <boogermanus@gmail.com>
2024-06-30 20:10:14 -0500

    base repository

--- API/Bibliotheca.Core/Interfaces/Database/IBaseRepository.cs
+++ API/Bibliotheca.Core/Interfaces/Database/IBaseRepository.cs
@@ -3,8 +3,8 @@ namespace Bibliotheca.Core.Interfaces.Database;
 public interface IBaseRepository<TEntity> where TEntity : class, IEntity
 {
     Task<IEnumerable<TEntity>> GetAllAsync();
-    Task<TEntity> GetAsync(int id);
+    Task<TEntity?> GetAsync(int id);
     Task<TEntity> AddAsync(TEntity entity);
-    Task<TEntity> UpdateAsync(TEntity entity);
-    Task<TEntity> DeleteAsync(int id);
+    Task<TEntity?> UpdateAsync(TEntity entity);
+    Task<TEntity?> DeleteAsync(int id);
 }
---
+++ API/Bibliotheca.Infrastrcture/Repositories/BaseRepository.cs
@@ -0,0 +1,64 @@
+using Bibliotheca.Core.Interfaces.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bibliotheca.Infrastrcture.Repositories;
+
+public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class, IEntity
+{
+    private readonly AppDbContext _context;
+    protected DbSet<TEntity> Entities => _context.Set<TEntity>();
+
+    protected AppDbContext DbContext => _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        await Entities.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<TEntity?> DeleteAsync(int id)
+    {
+        var existing = await GetAsync(id);
+
+        if(existing == null)
+            return null;
+
+        Entities.Remove(existing);
+
+        await _context.SaveChangesAsync();
+
+        return existing;
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await Entities.ToListAsync();
+    }
+
+    public async Task<TEntity?> GetAsync(int id)
+    {
+        return await Entities.FindAsync(id);
+    }
+
+    public async Task<TEntity?> UpdateAsync(TEntity entity)
+    {
+        var existing = await GetAsync(entity.Id);
+
+        if(existing == null)
+            return null;
+
+        _context.Entry(existing).CurrentValues.SetValues(entity);
+
+        Entities.Update(existing);
+
+        await _context.SaveChangesAsync();
+
+        return existing;
+    }
+}

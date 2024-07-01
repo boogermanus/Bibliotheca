c545581d3525c94d94bb9279dc259364310d0ca1
Jordan <boogermanus@gmail.com>
2024-06-30 20:31:49 -0500

    add library model

---
+++ API/Bibliotheca.Core/Models/Library.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Bibliotheca.Core.Interfaces.Database;
+
+namespace Bibliotheca.Core.Models;
+
+public class Library : IEntity
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreateDate { get; set; }
+    
+    
+}

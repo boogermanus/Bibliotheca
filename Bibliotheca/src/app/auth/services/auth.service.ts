f52b52fc25112d77d1ef31dadd135787f86d3183
Jordan <boogermanus@gmail.com>
2024-07-02 12:46:05 -0500

    home layout

--- Bibliotheca/src/app/auth/services/auth.service.ts
+++ Bibliotheca/src/app/auth/services/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
     private readonly httpClient: HttpClient,
     private readonly jwtService: JwtHelperService
   ) {
-    
+
     if (this.hasValidToken()) {
       this._isAuthenticated.set(true);
     }

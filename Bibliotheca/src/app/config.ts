aade717442ffc40d2fa32da1a1493e8c04a3cdd1
Jordan Woodruff <boogermanus@gmail.com>
2024-07-08 17:06:09 -0500

    config change

--- Bibliotheca/src/app/config.ts
+++ Bibliotheca/src/app/config.ts
@@ -1,5 +1,6 @@
 export const AppConfig = {
     production: false,
     authApi: 'http://localhost:5005/api/auth',
-    Api: 'http://localhost:5005/api/'
+    Api: 'http://localhost:5005/api/',
+    LibraryApi: 'http://localhost:5005/api/library'
 }

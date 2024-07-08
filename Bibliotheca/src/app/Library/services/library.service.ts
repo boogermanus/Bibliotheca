708dab54a4a66e70240b3d0ae518457515b9a67b
Jordan Woodruff <boogermanus@gmail.com>
2024-07-08 17:15:26 -0500

    more issues with user service

--- Bibliotheca/src/app/Library/services/library.service.ts
+++ Bibliotheca/src/app/Library/services/library.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ILibrary } from '../Interfaces/ilibrary';
+import { AppConfig } from '../../config';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +12,8 @@ export class LibraryService {
   constructor(
     private readonly httpClient: HttpClient
   ) { }
+
+  public getLibrariesForUser(): Observable<ILibrary[]> {
+    return this.httpClient.get<ILibrary[]>(`${AppConfig.LibraryApi}/GetLibrariesForUser`);
+  }
 }

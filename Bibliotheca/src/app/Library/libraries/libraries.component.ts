4b65bc992fe89b6a936a9160f80bb4f121db79d4
Jordan Woodruff <boogermanus@gmail.com>
2024-07-08 16:52:30 -0500

    library form

--- Bibliotheca/src/app/Library/libraries/libraries.component.ts
+++ Bibliotheca/src/app/Library/libraries/libraries.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,11 +16,26 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
-    MatListModule
+    MatListModule,
+    ReactiveFormsModule
   ],
   templateUrl: './libraries.component.html',
   styleUrl: './libraries.component.css'
 })
 export class LibrariesComponent {
+  public form: FormGroup
+  public libraryName: FormControl = new FormControl('', Validators.required)
 
+  constructor(
+    private readonly formBuilder: FormBuilder
+  )
+  {
+    this.form = this.formBuilder.group({
+      libraryName: this.libraryName
+    });
+  }
+
+  public submit(): void {
+    
+  }
 }

16776f902a4f489166ff5cbb59f00e834ecb1654
Jordan Woodruff <boogermanus@gmail.com>
2024-07-10 15:20:39 -0500

    basic layout for form

--- Bibliotheca/src/app/Library/library-users/library-users.component.ts
+++ Bibliotheca/src/app/Library/library-users/library-users.component.ts
@@ -1,18 +1,25 @@
-import { AfterContentInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ILibrary } from '../interfaces/ilibrary';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { LibraryService } from '../services/library.service';
 import { Observable, Subscription } from 'rxjs';
-import { MatListModule } from '@angular/material/list';
 import { ILibraryUser } from '../interfaces/ilibrary-user';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormField, MatInput, MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-library-users',
   standalone: true,
   imports: [
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
     MatListModule,
-    CommonModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './library-users.component.html',
   styleUrl: './library-users.component.css'

77057f173f8b4255d7b0b1c7e0006465b3c8b35d
Jordan Woodruff <boogermanus@gmail.com>
2024-07-10 17:14:53 -0500

    minor styling changes for bookshelves

--- Bibliotheca/src/app/Library/library-bookshelves/library-bookshelves.component.ts
+++ Bibliotheca/src/app/Library/library-bookshelves/library-bookshelves.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-library-bookshelves',
   standalone: true,
-  imports: [],
+  imports: [
+    MatListModule
+  ],
   templateUrl: './library-bookshelves.component.html',
   styleUrl: './library-bookshelves.component.css'
 })
--- Bibliotheca/src/app/Library/library-users/library-users.component.ts
+++ Bibliotheca/src/app/Library/library-users/library-users.component.ts
@@ -7,9 +7,10 @@ import { ActivatedRoute } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatFormField, MatInput, MatInputModule } from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpErrorResponse } from '@angular/common/http';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-library-users',
@@ -20,7 +21,8 @@ import { HttpErrorResponse } from '@angular/common/http';
     CommonModule,
     MatListModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTooltipModule
   ],
   templateUrl: './library-users.component.html',
   styleUrl: './library-users.component.css'

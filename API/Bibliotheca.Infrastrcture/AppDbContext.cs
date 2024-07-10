1b6d95de38c86432f0ffbf4f888aba43698d067d
Jordan Woodruff <boogermanus@gmail.com>
2024-07-10 18:09:08 -0500

    add bookshelf migration

--- API/Bibliotheca.Infrastrcture/AppDbContext.cs
+++ API/Bibliotheca.Infrastrcture/AppDbContext.cs
@@ -9,5 +9,6 @@ public class AppDbContext : IdentityDbContext<User>
 {
     public DbSet<Library> Libraries { get; set; }
     public DbSet<LibraryUser> LibraryUsers { get; set; }
+    public DbSet<LibraryBookshelf> LibraryBookshelves { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 }
---
+++ API/Bibliotheca.Infrastrcture/Migrations/20240710230545_LibraryBookshelf.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bibliotheca.Infrastrcture.Migrations
+{
+    /// <inheritdoc />
+    public partial class LibraryBookshelf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LibraryBookshelves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    NumberOfRows = table.Column<int>(type: "INTEGER", nullable: false),
+                    LibraryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibraryBookshelves", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LibraryBookshelves_Libraries_LibraryId",
+                        column: x => x.LibraryId,
+                        principalTable: "Libraries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryBookshelves_LibraryId",
+                table: "LibraryBookshelves",
+                column: "LibraryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LibraryBookshelves");
+        }
+    }
+}

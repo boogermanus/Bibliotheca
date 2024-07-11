b081308c339937b1925907e0a0a658b51d4fb964
Jordan Woodruff <boogermanus@gmail.com>
2024-07-11 18:06:51 -0500

    remove demo project

--- API/Bibliotheca.OpenLibrary/Models/Book.cs
+++ API/Bibliotheca.OpenLibrary/Models/Book.cs
@@ -3,4 +3,8 @@ namespace Bibliotheca.OpenLibrary.Models;
 public class Book
 {
     public string Title { get; set; } = string.Empty;
+    public override string ToString()
+    {
+        return Title;
+    }
 }

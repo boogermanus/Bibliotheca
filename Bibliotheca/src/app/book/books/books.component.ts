63886e98ec8dcb98dbcaa7e8382e6bdb7546d63a
Jordan Woodruff <boogermanus@gmail.com>
2024-07-16 13:04:16 -0500

    basic layout for books component

--- Bibliotheca/src/app/book/books/books.component.ts
+++ Bibliotheca/src/app/book/books/books.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTooltipModule
+  ],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })

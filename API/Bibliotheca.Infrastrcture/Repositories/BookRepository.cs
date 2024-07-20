224a52e42596e2c8410556a0207f57183ce51702
Jordan Woodruff <boogermanus@gmail.com>
2024-07-20 16:57:30 -0500

    first book query

--- API/Bibliotheca.Infrastrcture/Repositories/BookRepository.cs
+++ API/Bibliotheca.Infrastrcture/Repositories/BookRepository.cs
@@ -1,5 +1,6 @@
 using Bibliotheca.Core.Interfaces.Database.Repositories;
 using Bibliotheca.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bibliotheca.Infrastrcture.Repositories;
 
@@ -7,5 +8,19 @@ public class BookRepository : BaseRepository<Book>, IBookRepository
 {
     public BookRepository(AppDbContext context) : base(context)
     {
+    }
+
+    public async Task<IEnumerable<Book>> GetBooksForUserAsync(string userId)
+    {
+        var books = await Entities
+            .Join(DbContext.Libraries,
+                book => book.LibraryId,
+                library => library.Id,
+                (book,library) => new {book}
+            )
+            .Select(q => q.book)
+            .ToListAsync();
+
+        return books;
     }
 }

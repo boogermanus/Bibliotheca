815506a8bd5165869e453afcbb44299d74aec4e3
Jordan Woodruff <boogermanus@gmail.com>
2025-02-18 18:30:41 -0600

    http testing for fun

--- Bibliotheca/src/app/book/services/book.service.spec.ts
+++ Bibliotheca/src/app/book/services/book.service.spec.ts
@@ -2,20 +2,29 @@ import { TestBed } from '@angular/core/testing';
 
 import { BookService } from './book.service';
 import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { AppConfig } from '../../config';
 
 describe('BookService', () => {
   let service: BookService;
-
+  let http: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        provideHttpClient()
+        provideHttpClient(),
+        provideHttpClientTesting()
       ]
     });
     service = TestBed.inject(BookService);
+    http = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call getBookForUser HTTP', () => {
+    service.getBookForUser(1).subscribe();
+    http.expectOne({url: `${AppConfig.BookApi}/1`, method: 'GET'});
   });
 });

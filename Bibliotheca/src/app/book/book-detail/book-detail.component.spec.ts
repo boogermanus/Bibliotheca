378216b1f834002f40dea707d6d81e1718da0cc5
Jordan Woodruff <boogermanus@gmail.com>
2025-02-18 18:00:03 -0600

    test working the way it should

--- Bibliotheca/src/app/book/book-detail/book-detail.component.spec.ts
+++ Bibliotheca/src/app/book/book-detail/book-detail.component.spec.ts
@@ -10,24 +10,43 @@ describe('BookDetailComponent', () => {
   let component: BookDetailComponent;
   let fixture: ComponentFixture<BookDetailComponent>;
   beforeEach(async () => {
-
     const serviceSpy = jasmine.createSpyObj('BookService', ['getBookForUser']);
     await TestBed.configureTestingModule({
       imports: [BookDetailComponent],
       providers: [
         { provide: ActivatedRoute, useValue: { snapshot: { params: { id: 1 } } } },
-        {
-          provide: BookService, useValue: jasmine.createSpyObj('BookService', {
-            getBookForUser: of({ library: { name: '' }, libraryBookshelf: { name: '' } })
-          })
-        }
+        { provide: BookService, useValue: serviceSpy }
       ],
     })
       .compileComponents();
 
+    let service = TestBed.inject(BookService) as jasmine.SpyObj<BookService>;
+
+    const data: IBook = {
+      id: 1,
+      title: '',
+      author: '',
+      subject: '',
+      format: '',
+      isbn13: '',
+      isbn10: '',
+      numberOfPages: 1,
+      publishDate: new Date(),
+      description: '',
+      libraryId: 1,
+      library: { name: '', createDate: new Date(), bookCount: 1 },
+      libraryBookshelfId: 1,
+      libraryBookshelf: { name: '', libraryId: 1, numberOfRows: 1 },
+      row: 1
+    }
+    
+    service.getBookForUser.and.returnValue(of(data))
+    
     fixture = TestBed.createComponent(BookDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+
 
   });
 

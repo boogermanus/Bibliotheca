cf477a67bf321e0e3fa08be5ccea152e4cf2b8ad
Jordan Woodruff <boogermanus@gmail.com>
2025-08-06 14:54:12 -0500

    update demo program

--- API/Bibliotheca.OpenLibrary.Demo/Program.cs
+++ API/Bibliotheca.OpenLibrary.Demo/Program.cs
@@ -4,24 +4,6 @@ var isbnService = new IsbnOpenLibraryService();
 var authorService = new AuthorOpenLibraryService();
 var worksService = new WorksOpenLibraryService();
 var worksDescService = new WorkWithDescKeyService();
-// var book = await isbnService.GetBookAsync("9780140328721");
-// Console.WriteLine(book);
-
-// var author = await authorService.GetAuthorAsync(book?.Authors.First().Key ?? string.Empty);
-// Console.WriteLine(author);
-
-// var work = await worksService.GetWorkAsync(book?.Works.First().Key ?? string.Empty);
-// Console.WriteLine(work);
-
-// var book = await isbnService.GetBookAsync("067174559X");
-// Console.WriteLine(book);
-
-// var work = await worksService.GetWorkAsync(book?.Works.First().Key ?? string.Empty);
-// Console.WriteLine(work);
-
-// var workDescriptionKeyService = new WorkWithDescKeyService();
-// var workDk = await workDescriptionKeyService.GetWorkAsync(book?.Works.First().Key ?? string.Empty);
-// Console.Read();
 
 var service = new OpenLibraryService(isbnService, authorService, worksService, worksDescService);
 

465713853991e48f4eb3252dcd366f133b6bde8a
Jordan Woodruff <boogermanus@gmail.com>
2025-08-08 16:19:36 -0500

    fix testing

--- Bibliotheca/src/app/book/book-add-isbn-form/book-add-isbn-form.component.spec.ts
+++ Bibliotheca/src/app/book/book-add-isbn-form/book-add-isbn-form.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookAddIsbnFormComponent } from './book-add-isbn-form.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 describe('BookAddIsbnFormComponent', () => {
   let component: BookAddIsbnFormComponent;
@@ -8,7 +10,8 @@ describe('BookAddIsbnFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BookAddIsbnFormComponent]
+      imports: [BookAddIsbnFormComponent],
+      providers: [provideHttpClient(), { provide: ActivatedRoute, useValue: { snapshot: { params: { id: 1 } } } }]
     })
     .compileComponents();
 

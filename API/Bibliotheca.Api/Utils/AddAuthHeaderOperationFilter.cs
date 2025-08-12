6b651d0a815a45ff102fd211e864217b94c0d620
Jordan Woodruff <boogermanus@gmail.com>
2025-08-12 10:59:29 -0500

    minor formatting change

--- API/Bibliotheca.Api/Utils/AddAuthHeaderOperationFilter.cs
+++ API/Bibliotheca.Api/Utils/AddAuthHeaderOperationFilter.cs
@@ -7,8 +7,7 @@ public class AddAuthHeaderOperationFilter : IOperationFilter
 
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
-        if (operation.Security == null)
-            operation.Security = new List<OpenApiSecurityRequirement>();
+        operation.Security ??= new List<OpenApiSecurityRequirement>();
 
 
         var scheme = new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearer" } };

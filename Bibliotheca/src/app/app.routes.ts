c6295cce902b5fdf039a4c7b2b565ed36fa80e13
Jordan Woodruff <boogermanus@gmail.com>
2025-08-14 17:55:39 -0500

    fix #7 add lazy load and fix bug

--- Bibliotheca/src/app/app.routes.ts
+++ Bibliotheca/src/app/app.routes.ts
@@ -1,26 +1,45 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LibrariesComponent } from './library/libraries/libraries.component';
-import { authGuard } from './guards/auth.guard';
-import { LibraryViewComponent } from './library/library-view/library-view.component';
-import { BooksComponent } from './book/books/books.component';
-import { BookAddFormComponent } from './book/book-add-form/book-add-form.component';
-import { BookDetailComponent } from './book/book-detail/book-detail.component';
-import {BookAddIsbnFormComponent} from "./book/book-add-isbn-form/book-add-isbn-form.component";
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-    { path: 'libraries', component: LibrariesComponent, canActivate: [authGuard] },
-    { path: 'library/:id', component: LibraryViewComponent, canActivate: [authGuard] },
-    { path: 'books', component: BooksComponent, canActivate: [authGuard] },
-    { path: 'book-add-form', component: BookAddFormComponent, canActivate: [authGuard] },
-    {path: 'book-add-isbn-form', component: BookAddIsbnFormComponent, canActivate: [authGuard] },
-    { path: 'book-detail/:id', component: BookDetailComponent, canActivate: [authGuard] },
-    { path: '**', component: HomeComponent, pathMatch: 'full', redirectTo: '' }
+  {path: '', component: HomeComponent},
+  {path: 'login', loadComponent: () => import('./auth/login/login.component').then(r => r.LoginComponent)},
+  {path: 'register', loadComponent: () => import('./auth/register/register.component').then(r => r.RegisterComponent)},
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(r => r.DashboardComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'libraries',
+    loadComponent: () => import('./library/libraries/libraries.component').then(r => r.LibrariesComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'library/:id',
+    loadComponent: () => import('./library/library-detail/library-detail.component').then(r => r.LibraryDetailComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'books',
+    loadComponent: () => import('./book/books/books.component').then(r => r.BooksComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'book-add-form',
+    loadComponent: () => import('./book/book-add-form/book-add-form.component').then(r => r.BookAddFormComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'book-add-isbn-form',
+    loadComponent: () => import('./book/book-add-isbn-form/book-add-isbn-form.component').then(r => r.BookAddIsbnFormComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'book-detail/:id',
+    loadComponent: () => import('./book/book-detail/book-detail.component').then(r => r.BookDetailComponent),
+    canActivate: [authGuard]
+  },
+  {path: '**', component: HomeComponent, pathMatch: 'full', redirectTo: ''}
 ];
